d138719b0c6b17d229b5a420fd07e461bc089cbc
songhailei <csushl@163.com>
2024-10-20 16:59:05 +0800

    [feature]项目初始化配置mybatis,log4j2以及aop统一打印请求和返回信息

---
+++ src/main/java/com/imooc/mall/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.imooc.mall.controller;
+
+import com.imooc.mall.model.pojo.User;
+import com.imooc.mall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * description: 用户Controller
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/test")
+    @ResponseBody
+    public User personalPage() {
+        return userService.getUser();
+    }
+}
---
+++ src/main/java/com/imooc/mall/service/UserService.java
@@ -0,0 +1,11 @@
+package com.imooc.mall.service;
+
+import com.imooc.mall.model.pojo.User;
+
+/**
+ * Description: UserService
+ */
+public interface UserService {
+
+    User getUser();
+}
